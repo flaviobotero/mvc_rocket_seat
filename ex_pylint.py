b5b4cd4a15764a6c299f7d9b338aa44c47bcd30b
Flavio Mauricio Botero <flaviobotero@gmail.com>
2025-10-01 16:42:29 -0300

    Primeiro commit

---
+++ ex_pylint.py
@@ -0,0 +1,4 @@
+print("Hello word!")
+
+def minha_funcao():
+    print("Estou na minha funcao")
---
+++ src/models/sqlite/settings/connection_test.py
@@ -0,0 +1,13 @@
+import pytest
+from .connection import db_connection_handler
+from sqlalchemy.engine import Engine
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_connect_to_db():
+    assert db_connection_handler.get_engine() is None
+
+    db_connection_handler.connect_to_db()
+    db_engine = db_connection_handler.get_engine()
+
+    assert db_engine is not None
+    assert isinstance(db_engine, Engine)
